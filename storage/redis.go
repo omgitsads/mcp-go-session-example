8aad78be7cd18472cbf27e7a4b57eaf9a081c37a
Adam Holt <me@adamholt.co.uk>
2025-07-30 19:26:44 +0200

    Add comments for client info

--- storage/redis.go
+++ storage/redis.go
@@ -140,6 +140,9 @@ func (r *RedisSessionStore) Get(ctx context.Context, sessionID string) (*mcp.Str
 		return nil, fmt.Errorf("failed to connect session to server: %w", err)
 	}
 
+	// Re-initialize the session.
+	// Ideally we'll persist the client info as well from the actual initialize call, and re-hydrate it here.
+	// For now, we'll just leave it empty.
 	serverSession.Initialize(ctx, &mcp.InitializeParams{})
 
 	// Store the transport in the active sessions map
